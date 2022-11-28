31cc359efc2b3ae1ab2375c18534d152dcac0fbc
khang <khang.td194591@sis.hust.edu.vn>
2022-11-28 19:47:52 +0700

    update products

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,36 +1,38 @@
-import express from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
-import path from "path";
-import 'dotenv/config';
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import productsRouter from "./routes/products.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
 const URI = process.env.MDB_URI;
 
-
 /* ****** Middleware ****** */
-app.use(bodyParser.json({limit:'30mb'}));
-app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({
-    origin:true,
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
-mongoose.connect(URI)
+mongoose
+  .connect(URI)
 
-    .then(() =>{
-        app.listen(port, () => {
-            console.log("Connected to MongooseDB")
-            console.log(`Server is running on: http://localhost:${port}`);
-        });
-    })
-    .catch(err => {
-        console.log("err:", err);
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Connected to MongooseDB");
+      console.log(`Server is running on: http://localhost:${port}`);
     });
+  })
+  .catch((err) => {
+    console.log("err:", err);
+  });
 
 // app.use('/api/auth', auth);
 // app.use('/api/posts', posts);
@@ -38,14 +40,16 @@ mongoose.connect(URI)
 // app.use('/api/authorPost', authorPost);
 // app.use('/api/google', google);
 
-app.use('/api/users', users);   
-// app.use('/api/products', porducts);
+// app.use("/api/users", users);
+// // app.use('/api/products', porducts);
 
-/* API upload image */
-app.post('/api/upload', upload.single("file"), (req,res) => {
-    res.status(200).json("File has been uploaded");
+// /* API upload image */
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   res.status(200).json("File has been uploaded");
+// });
+
+app.get("/", (req, res) => {
+  res.json("heelo");
 });
 
-app.get('/', (req, res) => {
-    res.json("heelo");
-})
+app.use("/api/products", productsRouter);
--- BackEnd/models/Product.js
+++ BackEnd/models/Product.js
@@ -1,26 +1,29 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required:true
+      type: String,
+      required: true,
     },
     supplierId: {
-        type: Schema.Types.ObjectId,
-        ref: "Supplier_IT4492",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier_IT4492",
+      required: true,
     },
     description: {
-        type: String
+      type: String,
     },
     idImages: {
-        type: [Schema.Types.ObjectId],
+      type: [mongoose.Schema.Types.ObjectId],
     },
     status: {
-        type: String,
-        enum: ['active', 'inactive'],
-        default: 'active'
-    }
-}, { timestamps: true});
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+  },
+  { timestamps: true }
+);
 
 export const ProductModel = mongoose.model("Product_IT4492", ProductSchema);
